0e8ef65e046a3764f28c0ddbd3bec20cf690830e
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2023-11-16 14:30:35 -0500

    menu

---
+++ unity/Assets/Script/menu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+public void test()
+    {
+        Debug.Log("oh my god je fonctionne");
+    }
+}
